fb7b8c5ae2e8aa093387e29129d1d064a87618f6
cynthiakitur2021 <kiturcynthia02gmail.com>
2021-04-08 10:02:06 +0300

    changed index.html

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<b><title>WELCOME TO CLASS</title></b>
+</head>
+<!-- if this is to be read in a french dormain, you can make settings that the browser can interprate it to the french for example -->
+ <i>You should be able to <del>code</del>by now.</i>
+   <blockquote> Cardiovascular coding is challenging because of the need for detailed anatomical specificity both in the documentation and codes, the common existence of multiple comorbidities, and the frequent use of mechanical devices.</blockquote>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
